c2f70eacbaf8344f0fdfe4c84865c130e021b035
Aswinkumar2255 <mynameisaswin321@gmail.com>
2025-02-03 22:54:03 +0530

    feat : frontend pagination added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,6 +13,13 @@ function Home() {
   const [catloading, setcatloading] = useState(false);
   const [proloading, setproloading] = useState(false);
 
+  const [currentpage,setcurrentpage] = useState(0)
+
+  const PAGE_SIZE = 3;
+  const totalpage = Math.ceil(product.length/PAGE_SIZE)
+  const start = currentpage * PAGE_SIZE
+  const end = start + PAGE_SIZE
+
   
   
   useEffect(() => {
@@ -22,6 +29,17 @@ function Home() {
       setproloading(false)
     });
   }, []);
+
+  const handelpageclick = (page)=>{
+  setcurrentpage(page)
+
+  }
+  const handelprev = ()=>{
+    setcurrentpage((prev)=>prev-1)
+  }
+  const handelnext = ()=>{
+setcurrentpage((prev)=>prev + 1)
+  }
   return (
     <>
       <Navbar/>
@@ -39,10 +57,39 @@ function Home() {
             {" "}
             <div className="flex justify-center">
               <div className="lg:grid grid-cols-3 gap-4">
-                {product.map((e) => {
+                {product.slice(start,end).map((e) => {
                   return <Products key={e._id} item={e} />;
                 })}
               </div>
+            </div>
+            <div className="flex justify-center m-5 cursor-pointer">
+              {
+                currentpage !== 0 && <>
+                  <div  onClick={handelprev} className="flex p-1 border border-black m-1">
+                <span>PREV</span>
+                </div>
+                </>
+              }
+          
+             {
+              [...Array(totalpage).keys()].map((e,index)=>{
+                return <>
+                <div className="flex p-1 border border-black m-1 cursor-pointer">
+                <span onClick={()=>handelpageclick(e)} className={currentpage === e ? "bg-blue-500" : ""} key = {index}>{e + 1}</span>
+                </div>
+               
+                </>
+              })
+              
+             }
+             {
+              currentpage !== totalpage-1 && <>
+               <div onClick = {handelnext}className="flex p-1 border border-black m-1 cursor-pointer ">
+                <span>NEXT</span>
+                </div>
+              </>
+             }
+             
             </div>
           </>
         )}
